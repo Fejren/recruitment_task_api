8696fbf11196565fd17d8e3689e728d25f7efe72
Dawid Zaręba <68419282+Fejren@users.noreply.github.com>
2023-09-19 10:21:43 +0200

    Add AccountTier and UserProfile models. (#4)
    * Add AccountTier and UserProfile models
    
    * Indents in Image model

--- recruitment_task_api/user/admin.py
+++ recruitment_task_api/user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, UserProfile, AccountTier
 
 admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(AccountTier)
