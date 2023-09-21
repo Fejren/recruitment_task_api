c3fcd1052b6e790612364df0a01a28ea64f38331
Dawid Zaręba <dawid.zareba03@gmail.com>
2023-09-21 20:03:43 +0200

    Configure static urls

--- recruitment_task_api/recruitment_task_api/urls.py
+++ recruitment_task_api/recruitment_task_api/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,3 +8,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/images/', include('image.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
