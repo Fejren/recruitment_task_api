02362e6d6d4182f46dc20d8444b3a8f33897533c
Dawid Zaręba <dawid.zareba03@gmail.com>
2023-09-22 15:28:23 +0200

    Admin panels

--- recruitment_task_api/link/admin.py
+++ recruitment_task_api/link/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
+from .generate_expiring_link import is_link_expired
 from .models import ExpiringLink
 
-admin.site.register(ExpiringLink)
+
+@admin.register(ExpiringLink)
+class ExpiringLinkAdmin(admin.ModelAdmin):
+    list_filter = ['expires_at']
+    list_display = ('object_str', 'expires_at', 'is_expired')
+
+    def is_expired(self, obj):
+        return is_link_expired(obj)
+
+    def object_str(self, obj):
+        return str(obj)
+
+    is_expired.boolean = True  # Display as a boolean (checkbox)
+    is_expired.short_description = 'Expired'  # Custom column header
+    object_str.short_description = 'Link'
